576c6b3862b44cf4c05ecdcc8ff226028e55710f
Dayvedde <dayvedde@hotmail.com>
2015-07-19 19:53:08 -0400

    Basic cookie testing

--- resources/views/player/home.blade.php
+++ resources/views/player/home.blade.php
@@ -29,9 +29,35 @@
 @stop
 
 <script type='text/javascript'>
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        }
+        return "";
+    }
+    
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
     $(document).ready(function(){
         $('#actionBicepCurl').on('click', function(){
-           $("#infoBox").text("You just did 10 bicep curls!"); 
+            var bicepCurls = getCookie("bicepCurls");
+            if (bicepCurls !== 'NaN'){
+                bicepCurls = parseInt(bicepCurls) + 10;
+                setCookie("bicepCurls", bicepCurls, 365);
+            } else {
+                bicepCurls = 10;
+                setCookie("bicepCurls", 10, 365);
+            }
+           $("#infoBox").text("You just did " + bicepCurls + " bicep curls!"); 
         });
         $('#actionPushUp').on('click', function(){
            $("#infoBox").text("You just did 10 push ups!"); 
